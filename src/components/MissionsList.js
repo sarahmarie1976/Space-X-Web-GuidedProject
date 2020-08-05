e812d7ee2ee6031668b1037a80deef4f67b964eb
sarahmarie1976 <sholle7@gmail.com>
2020-08-04 20:40:20 -0500

    Guided Project Completed

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const MissionsList = props => {
+  // console.log(props.missions);
   return (
     <section className="missions-list">
       {props.error ? (
-        <div className="error">{props.error}</div>
+        <div  className="error">{props.error}</div>
       ) : (
         <div>
           {props.missions.map(mission => (
-            <div className="mission" key={mission.mission_id}>
+            <div data-testid="mission" className="mission" key={mission.mission_id}>
               {mission.mission_name}
             </div>
           ))}
